e341e18737013b3f1ddc10c7ccdcd8607a20ac1e
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-03 13:18:10 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+</head>
+
+<body>
+    <article>
+        <?php foreach ($posts as $postDate => $postsInDate){ ?>
+        <h2><?php echo $postDate ?></h2>
+        <ul>
+            <?php foreach($postsInDate as $postDate) { ?>
+            <li>
+                <?php echo($postDate["title"]) ?>
+                <?php echo($postDate["author"]) ?>
+                <?php echo($postDate["text"]) ?>
+            </li>
+            <?php } ?>
+        </ul>
+        <?php } ?>
+    </article>
+
+</body>
+
+</html>
