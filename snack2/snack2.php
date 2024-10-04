b5e7eb14b789f70cb227350c3083c91f068d46b4
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-04 12:07:46 +0200

    problem

--- snack2/snack2.php
+++ snack2/snack2.php
@@ -2,6 +2,13 @@
     $userName = $_GET["firstName"];
     $userMail = $_GET["mail"];
     $userAge = $_GET["age"];
+    $message = "";
+
+    if(str_contains($userMail, "@") && is_int($userAge)) {
+        $message = "Accesso Riuscito";
+    } else {
+        $message = "Accesso Negato";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack2</title>
 </head>
 
 <body>
@@ -24,6 +31,7 @@
         <input type="number" id="age" name="age" placeholder="age">
         <button type="submit">Invia</button>
     </form>
+    <h2><?= $message ?></h2>
 </body>
 
 </html>
