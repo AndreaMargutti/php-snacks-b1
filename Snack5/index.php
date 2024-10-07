1d71cd30564ab902f9ec2dc807a389f74008889e
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-07 12:17:52 +0200

    started snack5

---
+++ Snack5/index.php
@@ -0,0 +1,24 @@
+<?php 
+require_once (__DIR__ . "/../functions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Snack 5 - Functions PHP</h1>
+
+    <form action="./index.php" method="POST">
+        <label for="word">Inserisci la tua parola:</label>
+        <input type="text" name="word" id="word">
+        <button type="submit">Send</button>
+    </form>
+</body>
+
+</html>
